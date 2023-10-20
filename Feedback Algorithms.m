a6820d9e6d91de3d31686eaaa2ece7fe10c6ffff
Phillip Roos <55896656+philliproos01@users.noreply.github.com>
2023-10-20 03:56:39 -0400

    Update Feedback Algorithms.m

--- Feedback Algorithms.m
+++ Feedback Algorithms.m
@@ -2,16 +2,20 @@
 A = [-0.4 0 -0.01;1 0 0;-1.4 9.8 -0.02];
 B = [6.3; 0; 9.3]; 
 C = [0 0 1]%eye(3);
-sys_c = ss(A,B,C,0);
+sys = ss(A,B,C,0);
 
-step(sys_c)%, hold on, step(sys_d1,Tf)
+sys1 = ss2tf(A,B,C,D)
+step(sys)%, hold on, step(sys_d1,Tf)
 impulse(sys)
+
 
 %2
 A = [-0.0895 -0.286 0;-0.0439 -0.272 0;0 1 0];
 B = [0.0145; -0.0122; 0]; 
 C = [0 0 1]%eye(3);
 sys = ss(A,B,C,0);
+
+sys1 = ss2tf(A,B,C,D)
 
 step(sys)%, hold on, step(sys_d1,Tf)
 impulse(sys)
