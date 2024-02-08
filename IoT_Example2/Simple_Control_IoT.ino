84d3eaba59ec9f68089d2fd983136b2a88500eb2
Phillip Roos <55896656+philliproos01@users.noreply.github.com>
2024-02-08 11:01:51 -0500

    Corrected Movement commands

--- IoT_Example2/Simple_Control_IoT.ino
+++ IoT_Example2/Simple_Control_IoT.ino
@@ -1,9 +1,26 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
+//wheel parameters
+const unsigned long SPEED_TIMEOUT = 500000;   // Time used to determine wheel is not spinning
+const unsigned int UPDATE_TIME = 500;         // Time used to output serial data
+const unsigned int BUFFER_SIZE = 16;          // Serial receive buffer size
+const double BAUD_RATE = 115200;              // Serial port baud rate
+const double WHEEL_DIAMETER_IN = 6.5;            // Motor wheel diamater (inches)
+const double WHEEL_CIRCUMFERENCE_IN = 22.25;     // Motor wheel circumference (inches)
+const double WHEEL_DIAMETER_CM = 16.5;           // Motor wheel diamater (centimeters)
+const double WHEEL_CIRCUMFERENCE_CM = 56.5;      // Motor wheel circumference (centimeters)
+
+const int _pwmCtrl = 25;
+
 /* Put your SSID & Password */
 const char* ssid = "Hasta la vista, baby";  // Enter SSID here
 const char* password = "12345678";  //Enter Password here
+
+double _freq;               // Frequency of the signal on the speed pin
+double _rpm;                // Wheel speed in revolutions per minute
+double _mph;                // Wheel speed in miles per hour
+double _kph;                // Wheel speed in kilometers per hour
 
 /* Put IP Address details */
 IPAddress local_ip(192,168,1,1);
@@ -11,11 +28,27 @@ IPAddress gateway(192,168,1,1);
 IPAddress subnet(255,255,255,0);
 
 WebServer server(80);
+//left motor
+uint16_t motorDirection1 = 33;
+uint16_t motorDriver1 = 32;
 
-uint8_t LED1pin = 4;
+//right motor
+uint16_t motorDirection2 = 4; //set to 4
+uint16_t motorDriver2 = 26;
+
+bool _dir = 0;
+bool _dir2 = 1;
+
+
+const int SPEED = 25;
+const int PIN_SPEED = 14;
+const int PIN_SPEED2 = 12;
+
+
+uint8_t LED1pin = 18;
 bool LED1status = LOW;
 
-uint8_t LED2pin = 5;
+uint8_t LED2pin = 19;
 bool LED2status = LOW;
 
 bool forward_status = LOW;
@@ -27,18 +60,12 @@ bool spin_status = LOW;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED1pin, OUTPUT);
-  pinMode(LED2pin, OUTPUT);
-
+  while(!Serial);
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
   
   server.on("/", handle_OnConnect);
-  server.on("/led1on", handle_led1on);
-  server.on("/led1off", handle_led1off);
-  server.on("/led2on", handle_led2on);
-  server.on("/led2off", handle_led2off);
 
   //movement commands
   server.on("/forwardon", handle_forwardon);
@@ -57,104 +84,52 @@ void setup() {
   
   server.begin();
   Serial.println("HTTP server started");
+ // Set DIR pin to output
+    pinMode(motorDirection1, OUTPUT);
+    pinMode(motorDirection2, OUTPUT);
+    // Set PWM pin to output
+    pinMode(motorDriver1, OUTPUT);
+    pinMode(motorDriver2, OUTPUT);
+  
+    //feedback pin for motor 1
+    pinMode(PIN_SPEED, INPUT);
+    digitalWrite(motorDirection1, _dir);
+    digitalWrite(motorDirection2, _dir2);
+    analogWrite(motorDriver1, 0);
+    analogWrite(motorDriver2, 0);
+    Serial.println("Starting motor");
+
 }
 void loop() {
   server.handleClient();
-  if(LED1status)
-  {digitalWrite(LED1pin, HIGH);}
-  else
-  {digitalWrite(LED1pin, LOW);}
   
-  if(LED2status)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_forwardon)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_forwardoff)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_backwardon)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_backwardoff)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_lefton)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_leftoff)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_righton)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_rightoff)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_spinon)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-
-  if(handle_spinoff)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
+  if(forward_status) {
+      forward();
+  } else if(backward_status) {
+    backward();
+  } else if (left_status) {
+    left(50);
+  } else if (right_status) {
+    right(50);
+  } else if (spin_status) {
+    spin(); 
+  } else {
+    slowdown();
+  }
 }
 
 void handle_OnConnect() {
-  LED1status = LOW;
-  LED2status = LOW;
   forward_status = LOW;
   backward_status = LOW;
   left_status = LOW;
   right_status = LOW;
   spin_status = LOW;
   Serial.println("GPIO4 Status: OFF | GPIO5 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status,LED2status)); 
-}
-
-void handle_led1on() {
-  LED1status = HIGH;
-  Serial.println("GPIO4 Status: ON");
-  server.send(200, "text/html", SendHTML(true,LED2status)); 
-}
-
-void handle_led1off() {
-  LED1status = LOW;
-  Serial.println("GPIO4 Status: OFF");
-  server.send(200, "text/html", SendHTML(false,LED2status)); 
-}
-
-void handle_led2on() {
-  LED2status = HIGH;
-  Serial.println("GPIO5 Status: ON");
-  server.send(200, "text/html", SendHTML(LED1status,true)); 
-}
-
-void handle_led2off() {
-  LED2status = LOW;
-  Serial.println("GPIO5 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status,false)); 
+  server.send(200, "text/html", SendHTML(forward_status,false)); 
+  server.send(200, "text/html", SendHTML(backward_status,false)); 
+  server.send(200, "text/html", SendHTML(right_status,false)); 
+  server.send(200, "text/html", SendHTML(left_status,false)); 
+  server.send(200, "text/html", SendHTML(spin_status,false));  
 }
 
 void handle_forwardon() {
@@ -282,4 +257,142 @@ String SendHTML(uint8_t led1stat,uint8_t led2stat){
   ptr +="</body>\n";
   ptr +="</html>\n";
   return ptr;
+  }
+
+void forward() {
+    Serial.println("Going Forward");
+    digitalWrite(motorDirection1, 1);
+    digitalWrite(motorDirection2, 0);
+    delay(5);
+    analogWrite(motorDriver1, SPEED);
+    analogWrite(motorDriver2, SPEED);
+    
+    
+
 }
+
+void backward() {
+    Serial.println("Going Back");
+    digitalWrite(motorDirection1, LOW);
+    digitalWrite(motorDirection2, HIGH);
+    delay(5);
+    analogWrite(motorDriver1, SPEED);
+    analogWrite(motorDriver2, SPEED);
+    
+
+}
+
+void left(int period) {
+    Serial.println("Going Left");
+    digitalWrite(motorDirection1, 0);
+    digitalWrite(motorDirection2, 0);
+    delay(period);
+    analogWrite(motorDriver1, SPEED);
+    analogWrite(motorDriver2, SPEED);
+    
+    
+
+    //slowdown();
+}
+
+void right(int period) {
+    Serial.println("Going Right");
+    digitalWrite(motorDirection1, HIGH);
+    digitalWrite(motorDirection2, HIGH);
+    delay(period); 
+    analogWrite(motorDriver1, SPEED);
+    analogWrite(motorDriver2, SPEED);
+}
+
+
+void spin() {
+    Serial.println("Spinning");
+    digitalWrite(motorDirection1, 1);
+    digitalWrite(motorDirection2, 1);
+    delay(100);
+    analogWrite(motorDriver1, SPEED);
+    analogWrite(motorDriver2, SPEED);
+
+}
+
+
+//this is NOT a brake
+void slowdown() {
+    analogWrite(motorDriver1, 0);
+    analogWrite(motorDriver2, 0);
+    /*digitalWrite(motorDirection1, _dir);
+    digitalWrite(motorDirection2, _dir);
+    analogWrite(motorDriver1, 0);
+    analogWrite(motorDriver2, 0);*/
+
+}
+/*
+void ReadSpeed(int PIN)
+{
+    static bool lastState = false;    // Saves the last state of the speed pin
+    static unsigned long last_uS;     // The time (µs) when the speed pin changes
+    static unsigned long timeout_uS;  // Timer used to determine the wheel is not spinning
+
+    // Read the current state of the input pin
+    bool state = digitalRead(PIN);
+
+    // Check if the pin has changed state
+    if (state != lastState)
+    {
+      // Calculate how long has passed since last transition
+      unsigned long current_uS = micros();
+      unsigned long elapsed_uS = current_uS - last_uS;
+
+      // Calculate the frequency of the input signal
+      double period_uS = elapsed_uS * 2.0;
+      _freq = (1 / period_uS) * 1E6;
+
+      // Calculate the RPM
+      _rpm = _freq / 45 * 60;
+
+      // If RPM is excessively high then ignore it.
+      if (_rpm > 5000) _rpm = 0;
+
+      // Calculate the miles per hour (mph) based on the wheel diameter or circumference
+      //_mph = (WHEEL_DIAMETER_IN * PI * _rpm * 60) / 63360;
+      _mph = (WHEEL_CIRCUMFERENCE_IN * _rpm * 60) / 63360; 
+  
+      // Calculate the miles per hour (kph) based on the wheel diameter or circumference
+      //_kph = (WHEEL_DIAMETER_CM * PI * _rpm * 60) / 1000;
+      _kph = (WHEEL_CIRCUMFERENCE_CM * _rpm * 60) / 100000; 
+
+      // Save the last state and next timeout time
+      last_uS = current_uS;
+      timeout_uS = last_uS + SPEED_TIMEOUT;
+      lastState = state;
+    }
+    // If too long has passed then the wheel has probably stopped
+    else if (micros() > timeout_uS)
+    {
+        _freq = 0;
+        _rpm = 0;
+        _mph = 0;
+        _kph = 0;
+        last_uS = micros();
+    }
+}
+
+void WriteToSerial()
+{
+    // Local variables
+    static unsigned long updateTime;
+    
+    if (millis() > updateTime)
+    {
+        // Write data to the serial port
+        Serial.print((String)"Freq:" + _freq + " ");
+        Serial.print((String)"RPM:" + _rpm + " ");
+        Serial.print((String)"MPH:" + _mph + " ");
+        Serial.println((String)"KPH:" + _kph + " ");
+
+        // Calculate next update time
+        updateTime = millis() + UPDATE_TIME;
+    }
+}
+*/
+
