b5c648c168e7c3e483e3aef92cb89705e25da371
Alex Hu <alex.jt.hu@gmail.com>
2024-02-08 19:55:44 -0500

    Add PHP to form submission -- lat/longs now logged to file.
    To run this locally you'll need a PHP server set up.

---
+++ GUI/process.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $latitudes = array();
+    $longitudes = array();
+
+    for ($i = 1; $i <= 5; $i++) {
+        if (isset($_POST['latitude' . $i]) && isset($_POST['longitude' . $i])) {
+            $latitudes[] = $_POST['latitude' . $i];
+            $longitudes[] = $_POST['longitude' . $i];
+        }
+    }
+
+    $file = fopen("test/robot.txt", "w");
+    $coordinates = "";
+    for ($i = 0; $i < count($latitudes); $i++) {
+        $coordinates .= $latitudes[$i] . ", " . $longitudes[$i] . "\n";
+    }
+    fwrite($file, $coordinates);
+    fclose($file);
+    header("Location: index.html");
+    exit;
+}
+?>
