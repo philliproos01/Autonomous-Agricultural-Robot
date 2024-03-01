90284b84706cca168701e1c9eb189c30ab317339
Alex Hu <alex.jt.hu@gmail.com>
2024-03-01 12:09:50 -0500

    debug php file / log

--- GUI/process.php
+++ GUI/process.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('log_errors', 1);
+ini_set('error_log','error.log');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $latitudes = array();
     $longitudes = array();
